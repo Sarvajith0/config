f8e8f53c19675406832aaee9687513ccc3ddfc4e
Sarvajith <nsarvajith007@gmail.com>
2026-07-08 17:32:25 +0530

    fix cliphist

--- hypr/scripts/autostart.lua
+++ hypr/scripts/autostart.lua
@@ -7,12 +7,12 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("spotify")
     hl.exec_cmd("hypridle")
-    hl.exec_cmd("systemctl --user enable mpd.service")
-    hl.exefc_cmd("systemctl --user start mpd.service")
+    hl.exec_cmd("systemctl --user start mpd.service")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
+    -- hl.exec_cmd("wl-clip-persist --clipboard regular &")
     hl.exec_cmd("pkill -f xwaylandvideobridge")
-
+    hl.exec_cmd("systemctl --user enable mpd.service")
 end)
 
  hl.window_rule({
